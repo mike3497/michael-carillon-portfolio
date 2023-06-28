4a7b3dfd1157342944f1350fbf8599f192a7fbde
Michael Carillon <mcarillon97@gmail.com>
2023-06-28 18:56:08 -0400

    add FontAwesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: [
+    '~/assets/css/main.css',
+    '@fortawesome/fontawesome-free/css/fontawesome.min.css',
+    '@fortawesome/fontawesome-free/css/all.min.css',
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
