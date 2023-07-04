afc261e27ff82da5ac5dc868a1140524945bc5f9
Michael Carillon <mcarillon97@gmail.com>
2023-07-03 22:37:38 -0400

    update links to social media, email, and phone number

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,19 +2,21 @@
   <footer class="bg-zinc-900 text-white py-4">
     <div class="mb-4">
       <p class="text-center mb-2">
-        <a href="#"
+        <a href="mailto:mcarillon97@gmail.com"
           ><i class="fa-solid fa-envelope mr-2"></i>mcarillon97@gmail.com</a
         >
       </p>
       <p class="text-center">
-        <a href="#"><i class="fa-solid fa-phone mr-2"></i>(330) 703-7919</a>
+        <a href="tel:3307037919"
+          ><i class="fa-solid fa-phone mr-2"></i>(330) 703-7919</a
+        >
       </p>
     </div>
     <div class="flex flex-row gap-4 justify-center mb-4">
-      <CircleIconLink to="#">
+      <CircleIconLink to="https://github.com/mike3497">
         <i class="fa-brands fa-github"></i>
       </CircleIconLink>
-      <CircleIconLink to="#">
+      <CircleIconLink to="https://linkedin.com/in/michael-carillon">
         <i class="fa-brands fa-linkedin-in"></i>
       </CircleIconLink>
     </div>
@@ -23,5 +25,5 @@
 </template>
 
 <script setup lang="ts">
-import CircleIconLink from '~/components/shared/CircleIconLink';
+import CircleIconLink from '~/components/shared/CircleIconLink.vue';
 </script>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,8 +3,12 @@
     <div class="container mx-auto">
       <nav class="flex flex-row justify-between py-4">
         <div class="flex flex-row gap-4">
-          <a href="#"><i class="fa-brands fa-github"></i></a>
-          <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+          <a href="https://github.com/mike3497"
+            ><i class="fa-brands fa-github"></i
+          ></a>
+          <a href="https://linkedin.com/in/michael-carillon"
+            ><i class="fa-brands fa-linkedin-in"></i
+          ></a>
         </div>
         <div class="flex flex-row gap-4">
           <a href="#">About</a>
@@ -16,8 +20,8 @@
         <h1 class="font-bold uppercase text-4xl">Michael Carillon</h1>
         <h2>Full-stack Web Developer</h2>
         <div class="flex flex-row gap-4 text-sm">
-          <a href="#">mcarillon97@gmail.com</a>
-          <a href="#">(330) 703-7919</a>
+          <a href="mailto:mcarillon97@gmail.com">mcarillon97@gmail.com</a>
+          <a href="tel:3307037919">(330) 703-7919</a>
         </div>
       </div>
     </div>
