55757affcbd3f8c821d289a8c9c94df8afecf995
Michael Carillon <mcarillon97@gmail.com>
2024-07-18 22:27:42 -0400

    fix: Resume PDF button responsiveness

--- components/experience/MyExperience.vue
+++ components/experience/MyExperience.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="experience" class="container my-4">
     <SectionHeading>//Experience</SectionHeading>
-    <div class="grid grid-cols-1 md:grid-cols-5">
+    <div class="grid grid-cols-1 md:grid-cols-5 gap-4">
       <div class="col-span-2">
         <ul class="mb-4">
           <li
