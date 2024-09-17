a91701989d90068bb0a46cff1e1ffa3b77da6295
Michael Carillon <mcarillon97@gmail.com>
2024-09-16 22:33:23 -0400

    feat: update date, remove phone number, and update semantics in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,32 +1,36 @@
 <template>
   <footer class="bg-zinc-900 text-white py-4">
-    <div class="flex justify-center gap-4 mb-4">
-      <a href="mailto:mcarillon97@gmail.com">
-        <i class="fa-solid fa-envelope mr-2"></i>
-        <span>mcarillon97@gmail.com</span>
-      </a>
-      <a href="tel:3307037919">
-        <i class="fa-solid fa-phone mr-2"></i>
-        <span>(330) 703-7919</span>
-      </a>
-    </div>
-    <div class="flex flex-row gap-4 justify-center mb-4">
-      <CircleIconLink
-        href="https://github.com/mike3497"
-        aria-label="Michael Carillon Github"
-        :variant="CircleIconLinkVariant.WHITE"
-      >
-        <i class="fa-brands fa-github"></i>
-      </CircleIconLink>
-      <CircleIconLink
-        href="https://linkedin.com/in/michael-carillon"
-        aria-label="Michael Carillon LinkedIn"
-        :variant="CircleIconLinkVariant.WHITE"
-      >
-        <i class="fa-brands fa-linkedin-in"></i>
-      </CircleIconLink>
-    </div>
-    <p class="text-center text-xs">© Copyright {{ year }} Michael Carillon</p>
+    <ul class="flex justify-center gap-4 mb-4">
+      <li>
+        <a href="mailto:mcarillon97@gmail.com" class="flex items-center">
+          <i class="fa-solid fa-envelope mr-2"></i>
+          <span>mcarillon97@gmail.com</span>
+        </a>
+      </li>
+    </ul>
+    <ul class="flex flex-row gap-4 justify-center mb-4">
+      <li>
+        <CircleIconLink
+          href="https://github.com/mike3497"
+          aria-label="Michael Carillon Github"
+          :variant="CircleIconLinkVariant.WHITE"
+        >
+          <i class="fa-brands fa-github"></i>
+        </CircleIconLink>
+      </li>
+      <li>
+        <CircleIconLink
+          href="https://linkedin.com/in/michael-carillon"
+          aria-label="Michael Carillon LinkedIn"
+          :variant="CircleIconLinkVariant.WHITE"
+        >
+          <i class="fa-brands fa-linkedin-in"></i>
+        </CircleIconLink>
+      </li>
+    </ul>
+    <p class="text-center text-xs">
+      © Copyright 2023 - {{ year }} Michael Carillon
+    </p>
   </footer>
 </template>
 
@@ -34,5 +38,5 @@
 import CircleIconLink from '@/components/shared/CircleIconLink.vue';
 import { CircleIconLinkVariant } from '~/types/circleIconLinkVariant';
 
-const year = computed<number>(() => new Date().getFullYear());
+const year = ref<number>(new Date().getFullYear());
 </script>
